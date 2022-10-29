e8a5b71dabf893800790301dcec47d432d5b20ed
JUSTBOREDGAMESest2022 <112311643+JUSTBOREDGAMESest2022@users.noreply.github.com>
2022-10-29 23:01:33 +0530

    Add files via upload

---
+++ website/chat.js
@@ -0,0 +1,10 @@
+class InteractiveChatbox {
+    constructor(a, b, c) {
+        this.args = {
+            button: a,
+            chatbox: b
+        }
+        this.icons = c;
+        this.state = false; 
+    }
+}
